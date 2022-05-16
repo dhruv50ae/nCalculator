e8a55ad2a79ece7c8561edf367ef836522c384d6
dhruv50ae <linkdp2012@gmail.com>
2022-05-16 14:18:53 +0100

    Added all files

---
+++ calculator.js
@@ -0,0 +1,13 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(3000, () => {
+  console.log("Server has started");
+});
